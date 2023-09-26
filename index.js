17c8191195cbc2c533883b5b402ebfb702bd9aea
FehRoque <feliperamosroque@gmail.com>
2023-09-26 16:50:52 -0400

    connectToMongo()

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const { connectToMongo } = require("./src/config/mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,4 +10,5 @@ app.get("/", (_, res) => {
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
+  connectToMongo();
 });
