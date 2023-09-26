679f5e11f71c64a5eeb0a0d052611993f09340be
FehRoque <feliperamosroque@gmail.com>
2023-09-26 18:07:41 -0400

    add middlewares: isOwner and isAuthenticaded

---
+++ src/middlewares/index.js
@@ -0,0 +1,47 @@
+const { get, merge } = require("lodash");
+const { getUserBySessionToken } = require("../models/user");
+
+const isOwner = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id");
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+const isAuthenticaded = async (req, res, next) => {
+  try {
+    const sessionToken = req.cookies["EPILEF-AUTH"];
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+module.exports = {
+  isAuthenticaded,
+  isOwner,
+};
