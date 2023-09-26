ef9a0bd91f9c6c1af90e2b17b58008887cadf78d
FehRoque <feliperamosroque@gmail.com>
2023-09-26 19:04:16 -0400

    add authentication router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
+const authentication = require("./authentication");
 const users = require("./users");
 
 module.exports = () => {
+  authentication(router)
   users(router);
   return router;
 };
