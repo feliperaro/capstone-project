02a48f307d324850c4f9180447413ebcfe6fcfb9
FehRoque <feliperamosroque@gmail.com>
2023-09-26 23:19:17 -0400

    compression middleware removed

--- index.js
+++ index.js
@@ -2,21 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const router = require("./src/router");
 const cookieParser = require("cookie-parser");
-const compression = require("compression");
 const { connectToMongo } = require("./src/config/mongoose");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// Middleware
-
-app.use(compression());
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
 
 app.use("/", router());
-
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
   connectToMongo();
